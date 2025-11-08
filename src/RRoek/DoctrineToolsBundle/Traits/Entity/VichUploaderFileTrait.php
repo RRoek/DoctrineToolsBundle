e5443df5c744bd3e237714942c471d26f0fa3ff9
R. Roek <35526527+RRoek@users.noreply.github.com>
2025-11-08 11:10:54 +0100

    ft(#DoctrineToolsBundle): Replace annotation by attributes for files

--- src/RRoek/DoctrineToolsBundle/Traits/Entity/VichUploaderFileTrait.php
+++ src/RRoek/DoctrineToolsBundle/Traits/Entity/VichUploaderFileTrait.php
@@ -8,45 +8,51 @@ use Symfony\Component\Validator\Constraints as Assert;
 
 trait VichUploaderFileTrait
 {
-    /**
-     * @ORM\Column(type="string", length=255)
-     * @var string
-     */
-    private $file;//link of file stored in db
+    #[ORM\Column(nullable: true)]
+    private ?string $file = null;//link of file stored in db
 
-    /**
-     * @Vich\UploadableField(mapping="uploaded_files", fileNameProperty="file")
-     * @var File
-     * @Assert\File()
-     */
+    #[ORM\Column(nullable: true)]
+    private ?int $filedSize = null;
+
+    #[Vich\UploadableField(mapping: 'uploaded_files', fileNameProperty: 'file', size: 'fileSize')]
     private $fileDiskFile;//stores the binary contents of the file file and it's not persisted in the database (that's why it doesn't define a @ORM annotation).
 
     //---- ---  --- ----
-    public function setFileDiskFile(File $file = null)
+    public function setFileDiskFile(?File $file = null): void
     {
         $this->fileDiskFile = $file;
 
         // VERY IMPORTANT:
         // It is required that at least one field changes if you are using Doctrine,
         // otherwise the event listeners won't be called and the file is lost
-        if ($file) {
+        if (null !== $file) {
             // if 'updatedAt' is not defined in your entity, use another property
             $this->updatedAt = new \DateTime('now');
         }
     }
 
-    public function getFileDiskFile()
+    public function getFileDiskFile(): ?File
     {
         return $this->fileDiskFile;
     }
 
-    public function setFile($file)
+    public function setFile(?string $file): void
     {
         $this->file = $file;
     }
 
-    public function getFile()
+    public function getFile(): ?string
     {
         return $this->file;
+    }
+
+    public function setFileSize(?int $fileSize): void
+    {
+        $this->fileSize = $fileSize;
+    }
+
+    public function getFileSize(): ?int
+    {
+        return $this->fileSize;
     }
 }
--- src/RRoek/DoctrineToolsBundle/Traits/Entity/VichUploaderImageTrait.php
+++ src/RRoek/DoctrineToolsBundle/Traits/Entity/VichUploaderImageTrait.php
@@ -8,45 +8,51 @@ use Symfony\Component\Validator\Constraints as Assert;
 
 trait VichUploaderImageTrait
 {
-    /**
-     * @ORM\Column(type="string", length=255)
-     * @var string
-     */
-    private $image;//link of image stored in db
+    #[ORM\Column(nullable: true)]
+    private ?string $image = null;//link of image stored in db
 
-    /**
-     * @Vich\UploadableField(mapping="uploaded_images", fileNameProperty="image")
-     * @var File
-     * @Assert\Image()
-     */
+    #[ORM\Column(nullable: true)]
+    private ?int $imageSize = null;
+
+    #[Vich\UploadableField(mapping: 'uploaded_images', fileNameProperty: 'image', size: 'imageSize')]
     private $imageFile;//stores the binary contents of the image file and it's not persisted in the database (that's why it doesn't define a @ORM annotation).
 
     //---- ---  --- ----
-    public function setImageFile(File $image = null)
+    public function setImageFile(?File $image = null): void
     {
         $this->imageFile = $image;
 
         // VERY IMPORTANT:
         // It is required that at least one field changes if you are using Doctrine,
         // otherwise the event listeners won't be called and the file is lost
-        if ($image) {
+        if (null !== $image) {
             // if 'updatedAt' is not defined in your entity, use another property
             $this->updatedAt = new \DateTime('now');
         }
     }
 
-    public function getImageFile()
+    public function getImageFile(): ?File
     {
         return $this->imageFile;
     }
 
-    public function setImage($image)
+    public function setImage(?string $image): void
     {
         $this->image = $image;
     }
 
-    public function getImage()
+    public function getImage(): ?string
     {
         return $this->image;
+    }
+
+    public function setImageSize(?int $imageSize): void
+    {
+        $this->imageSize = $imageSize;
+    }
+
+    public function getImageSize(): ?int
+    {
+        return $this->imageSize;
     }
 }
